735c46fbed9ca2a02e2f83d28f076de4af80e19c
neevparikh <neev_parikh@brown.edu>
2020-02-10 09:12:06 -0500

    Moved to using ghc command args

--- compile.sh
+++ compile.sh
@@ -5,19 +5,4 @@
 ########################################
 
 # Update this file with instructions on how to compile your code
-ghc src/solver.hs
-
-mkdir -p bin/
-if test -f "src/solver"; then
-	mv src/solver bin/
-fi
-if test -f "src/solver.exe"; then
-	mv src/solver.exe bin/
-fi
-if test -f "src/solver.o"; then
-	mv src/solver.o bin/
-fi
-if test -f "src/solver.hi"; then
-	mv src/solver.hi bin/
-fi
-
+ghc -dynamic -c src/solver.hs -o bin/solver -odir bin/ -hidir bin/
